236146dbcf59d6af79ac5ecd064e035d4831a2fb
wendraw <252798869@qq.com>
2020-09-20 14:16:26 +0800

    commit 1

--- index/index.js
+++ index/index.js
@@ -2,10 +2,10 @@ const app = getApp()
 
 Page({
   data: {
-
+    title: "9 月 21 日起，暴风集团股票交易进入退市整理期",
+    preContent: "暴风集团发布公告称，公司于 2020 年 8 月 28 日收到深交所《关于暴风集团股份有限公司终止上市的决定》，自",
+    time: "9 月 21 日",
+    posContent:"起，公司股票交易进入退市整理，在退市整理期 30 个交易日后公司将为摘牌...根据公告，公司股票在退市整理期交易期间，公司将不筹划、不进行重大资产重组事项。"
   },
-  onLoad: function () {
-    console.log('代码片段是一种迷你、可分享的小程序或小游戏项目，可用于分享小程序和小游戏的开发经验、展示组件和 API 的使用、复现开发问题和 Bug 等。可点击以下链接查看代码片段的详细文档：')
-    console.log('https://mp.weixin.qq.com/debug/wxadoc/dev/devtools/devtools.html')
-  },
+  onLoad: function () {},
 })
