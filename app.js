5ea8c5097420583cf7e34df16a083ae081bea5ad
wendraw <252798869@qq.com>
2020-09-21 13:38:24 +0800

    commit finished

--- app.js
+++ app.js
@@ -1,5 +1,26 @@
+let config = require('config/config');
+
 App({
   onLaunch: function () {
-
+    try {
+      let res = wx.getSystemInfoSync();
+      config.pixelRate = res.windowWidth / 750;
+      config.platform = res.platform;
+      config.statusBarHeight = res.statusBarHeight;
+      if (res.platform.toLowerCase() == 'devtools') {
+        config.capsuleHeight = 44;
+      }
+      if (res.platform.toLowerCase() == 'android') {
+        config.capsuleHeight = 48;
+      }
+      config.titleHeight = (config.capsuleHeight + config.statusBarHeight) / config.pixelRate;
+      if (res.statusBarHeight >= 44) {
+        config.isHighHead = true;
+      }
+      if (res.windowHeight > 750) config.isAllScreen = true;
+      config.systemHeight = res.windowHeight;
+    } catch (e) {
+      console.log(e);
+    }
   }
 })
--- index/index.js
+++ index/index.js
@@ -1,3 +1,5 @@
+const config = require('../config/config');
+
 const app = getApp()
 
 const date = new Date();
@@ -46,19 +48,24 @@ Page({
     title: "9 月 21 日起，暴风集团股票交易进入退市整理期",
     preContent: "暴风集团发布公告称，公司于 2020 年 8 月 28 日收到深交所《关于暴风集团股份有限公司终止上市的决定》，自",
     time: "9 月 21 日",
-    posContent: "起，公司股票交易进入退市整理，在退市整理期 30 个交易日后公司将为摘牌...根据公告，公司股票在退市整理期交易期间，公司将不筹划、不进行重大资产重组事项。",
+    posContent: "起，公司股票交易进入退市整理，在退市整理期 30 个交易日后公司将为摘牌 ... 根据公告，公司股票在退市整理期交易期间，公司将不筹划、不进行重大资产重组事项。",
     eventTitle: "9 月 21 日起，暴风集团股票交易进入退市整理期",
-    timerPickerShow: false,
+    timerPickerShow: true,
     popupShow: false,
 
     days: days,
     hours: hours,
     minutes: minutes,
     timerIndexs: [0, 0, 0],
+
+    isAllScreen: config.isAllScreen
   },
 
   onLoad: function () {
-
+    const query = wx.createSelectorQuery().in(this);
+    query.select('#my-popup').boundingClientRect((res) => {
+      console.log(res)
+    }).exec()
   },
 
   subscribeReminder: function (e) {
@@ -101,11 +108,12 @@ Page({
   showMyPopup: function (e) {
     const query = wx.createSelectorQuery().in(this);
     query.select('#my-popup').boundingClientRect((res) => {
+      console.log(res)
       this.animate('#my-popup', [{
         translateY: 0,
       }, {
-        translateY: -res.height,
-      }], 200)
+        translateY: -1000,
+      }], 300)
     }).exec()
   },
 
@@ -116,25 +124,14 @@ Page({
     const query = wx.createSelectorQuery().in(this);
     query.select('#my-popup').boundingClientRect((res) => {
       this.animate('#my-popup', [{
-        translateY: -res.height,
+        translateY: -1000,
       }, {
         translateY: 0,
-      }], 200)
-    }).exec()
+      }], 300)
+    }).exec();
   },
 
-  showTimerPicker: function (e) {},
-
-  hideTimerPicker: function (e) {
-    // const query = wx.createSelectorQuery().in(this);
-    // query.select('#timer-picker').boundingClientRect((res) => {
-    //   console.log(res);
-    //   this.animate('#timer-picker', [{
-    //     height: res.height,
-    //   }, {
-    //     height: 0,
-    //   }], 200)
-    // }).exec();
+  onTimerPickerTap: function (e) {
     this.setData({
       timerPickerShow: !this.data.timerPickerShow,
     })
